c10b53bbc6c247a9e78750cea8137011f0cb41a9
Dima <57713733+Dima1409@users.noreply.github.com>
2022-10-21 13:22:25 +0300

    render stats

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,22 @@
 import user from '../incomingData/user.json';
+import data from '../incomingData/data.json'
 import {Profile} from 'components/Profile/Profile'
+import { Statistics } from './Statistics/Statistics';
 
 export const App = () => {
   return (
-   <Profile username={user.username} 
+    <>   
+    <Profile username={user.username} 
             tag={user.tag} 
             location={user.location} 
             avatar={user.avatar}
             followers={user.stats.followers}
             views={user.stats.views}
             likes={user.stats.likes}/>
+
+    <Statistics title="Upload stats"  stats={data}/>
+    <Statistics stats={data}/>
+    </>
+
   );
 };
