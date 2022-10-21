26ccd9c215d4578d89af3db17f85935ff16f87c0
Dima <57713733+Dima1409@users.noreply.github.com>
2022-10-21 14:00:37 +0300

    task 1.2

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,20 +1,30 @@
 import css from './Statistics.module.css';
 import { getRandomHexColor } from 'utils/getRandomColor';
-//import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 export const Statistics = ({title, stats}) => {
-    return (<section className={css.statistics}>
+    return <section className={css.statistics}>
     {title ? (<h2 className={css.title}>{title}</h2>) : null}
 
     <ul className={css.statList}>
         {stats.map(({id, label, percentage}) => (
             <li key={id} className={css.item} style={{backgroundColor: `${getRandomHexColor()}`}}>
-        <span className={css.label}>{label}%</span>
+        <span className={css.label}>{label}</span>
         <span className={css.percentage}>{percentage}%</span>
              </li>
         )        
         )}
     </ul>
-  </section>)
+  </section>
 }
+
+Statistics.propTypes = {
+    title: PropTypes.string,
+    stats: PropTypes.exact({
+        id: PropTypes.string.isRequired,
+        label: PropTypes.string.isRequired,
+        percentage: PropTypes.number.isRequired
+    })
+}
+
 
