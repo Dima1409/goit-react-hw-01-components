9f4c049fa699971589f4ae91610f773ab8af128d
Dima <57713733+Dima1409@users.noreply.github.com>
2022-10-22 00:17:43 +0300

    task4

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,22 +1,34 @@
-import css from './TransactionHistory.module.css'
+import css from './TransactionHistory.module.css';
+import PropTypes from 'prop-types';
 
 export const TransactionHistory = ({items}) => {
     return <table className={css.transactionHistory}>
     <thead>
-      <tr>
-        <th>Type</th>
-        <th>Amount</th>
-        <th>Currency</th>
+      <tr className={css.transactionRowHead}>
+        <th className={css.transactionTh}>Type</th>
+        <th className={css.transactionTh}>Amount</th>
+        <th className={css.transactionTh}>Currency</th>
       </tr>
     </thead>
+    <tbody>
     {items.map(({id, type, amount, currency}) => (
-        <tbody>
-        <tr key={id}>
-          <td>{type}</td>
-          <td>{amount}</td>
-          <td>{currency}</td>
+        <tr className={css.transactionBodyRow} key={id}>
+          <td className={css.transactionBodyTd}>{type}</td>
+          <td className={css.transactionBodyTd}>{amount}</td>
+          <td className={css.transactionBodyTd}>{currency}</td>
         </tr>
-      </tbody>
     ))}
+    </tbody>
   </table>
+}
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired
+    })
+  )
 }
